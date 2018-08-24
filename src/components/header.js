90a82d72188b252e9b9d78f7b430a5151b1fdcd8
splnlss <jon@splnlss.com>
2018-08-24 14:27:09 -0400

    added filemanegement components

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './dashboard.css'
 import './header.css'
 import logo from '../images/logo.svg';
@@ -6,21 +7,23 @@ import logo from '../images/logo.svg';
 export default function Header(props){
 
 return (
-  <div>
-      <header className="header">
-        <img src={logo} className="logo" alt="Chronic Data Logo" height="120"/>
-        <div className="title">
-          <h1 className="header-title">CHRONIC DATA SOCIETY</h1>
-          <p className="header-intro">
-              your data. your control.
-          </p>
-        </div>
-			<nav>
-				<ul className="header-links">
-					<li><a className="new" onClick={props.addDocument}>+ Add Medical Document</a></li>
-				</ul>
-			</nav>
-		</header>
-  </div>
+  <Router>
+    <div>
+        <header className="header">
+          <img src={logo} className="logo" alt="Chronic Data Logo" height="120"/>
+          <div className="title">
+            <h1 className="header-title"><Link to="/">CHRONIC DATA SOCIETY</Link></h1>
+            <p className="header-intro">
+                your data. your control.
+            </p>
+          </div>
+  			<nav>
+  				<ul className="header-links">
+  					<li><a className="new" onClick={props.addDocument}>+ Add Medical Document</a></li>
+  				</ul>
+  			</nav>
+  		</header>
+    </div>
+  </Router>
     )
 }
