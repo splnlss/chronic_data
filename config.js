ca1218178ead69fd94166e927e35b8726e4b75a6
splnlss <jon@splnlss.com>
2018-08-27 23:50:43 -0400

    added mongodb

---
+++ config.js
@@ -0,0 +1,10 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/bestseat-app'
+
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+  'mongodb://localhost/test-bestseat-app'
+exports.PORT = process.env.PORT || 8080
+// exports.JWT_SECRET = process.env.JWT_SECRET || 'secret'
+// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
+
+console.log(`DATABASE_URL = ${exports.DATABASE_URL}`)
+// should be in config heroku
--- server.js
+++ server.js
@@ -1,71 +1,79 @@
-const express = require("express");
-const fs = require("fs");
-const sqlite = require("sql.js");
+const express = require('express')
 
-const filebuffer = fs.readFileSync("db/usda-nnd.sqlite3");
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+// const passport = require('passport')
+mongoose.Promise = global.Promise
 
-const db = new sqlite.Database(filebuffer);
+const {DATABASE_URL, PORT} = require('./config')
+const {router:reviewRouter} = require('./review')
+const {router:userRouter} = require('./user')
+const {router:yelpRouter} = require('./yelp')
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+// require models
 
-const app = express();
+const app = express()
+let server
 
-app.set("port", process.env.PORT || 3001);
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// passport.use(localStrategy)
+// passport.use(jwtStrategy)
+
+// app.use('/api/user', userRouter)
+// app.use('/api/review/', reviewRouter)
+// app.use('/api/auth/', authRouter)
+// app.use('/api/yelp', yelpRouter)
+
+//middleware to recognize jwt
+// const jwtAuth = passport.authenticate('jwt', { session: false })
+
+//route to find user by jwt
+// app.post('/api/userSearch', jwtAuth, (req, res)=>{
+//   console.log(req.user)
+//   return req.user
+// })
+
+function runServer(databaseURL = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseURL, err => {
+      if (err) {
+        return reject(err)
+      }
+      server = app.listen(port, () => {
+          console.log(`You're app is listening on ${port}`)
+          resolve()
+        })
+        .on('error', err => {
+          mongoose.disconnect()
+          reject(err)
+        })
+    })
+  })
 }
 
-const COLUMNS = [
-  "carbohydrate_g",
-  "protein_g",
-  "fa_sat_g",
-  "fa_mono_g",
-  "fa_poly_g",
-  "kcal",
-  "description"
-];
-app.get("/api/food", (req, res) => {
-  const param = req.query.q;
-
-  if (!param) {
-    res.json({
-      error: "Missing required parameter `q`"
-    });
-    return;
-  }
-
-  // WARNING: Not for production use! The following statement
-  // is not protected against SQL injections.
-  const r = db.exec(
-    `
-    select ${COLUMNS.join(", ")} from entries
-    where description like '%${param}%'
-    limit 100
-  `
-  );
-
-  if (r[0]) {
-    res.json(
-      r[0].values.map(entry => {
-        const e = {};
-        COLUMNS.forEach((c, idx) => {
-          // combine fat columns
-          if (c.match(/^fa_/)) {
-            e.fat_g = e.fat_g || 0.0;
-            e.fat_g = (parseFloat(e.fat_g, 10) +
-              parseFloat(entry[idx], 10)).toFixed(2);
-          } else {
-            e[c] = entry[idx];
-          }
-        });
-        return e;
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('closing server')
+      server.close(err => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
       })
-    );
-  } else {
-    res.json([]);
-  }
-});
+    })
+  })
+}
 
-app.listen(app.get("port"), () => {
-  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
-});
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err))
+}
+
+module.exports = {
+  runServer, app, closeServer
+}
