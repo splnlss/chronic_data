a78af1d6371724d6795f5f7e15628b1c2c6c62e5
Splnlss <splnlss@Splnlsss-MBP.T-mobile.com>
2018-10-22 23:31:54 -0400

    added user login

--- server.js
+++ server.js
@@ -1,80 +1,98 @@
-const express = require('express')
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
 
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-// const passport = require('passport')
-mongoose.Promise = global.Promise
+// Here we use destructuring assignment with renaming so the two variables
+// called router (from ./users and ./auth) have different names
+// For example:
+// const actorSurnames = { james: "Stewart", robert: "De Niro" };
+// const { james: jimmy, robert: bobby } = actorSurnames;
+// console.log(jimmy); // Stewart - the variable name is jimmy, not james
+// console.log(bobby); // De Niro - the variable name is bobby, not robert
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
-const {DATABASE_URL, PORT} = require('./config')
-// const {router:index} = require('./')
- // const {router} = require('/api/routes/')
-// const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+mongoose.Promise = global.Promise;
 
+const { PORT, DATABASE_URL } = require('./config');
 
-const app = express()
-let server
+const app = express();
 
-app.use(morgan('common'))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+// Logging
+app.use(morgan('common'));
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.get('/', function(req, res) {
-//     res.json({ message: 'hooray! welcome to our api!' });
-// });
-//
-// app.use('/api', index);
-// app.use('/api/', users);
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
 
-//middleware to recognize jwt
-// const jwtAuth = passport.authenticate('jwt', { session: false })
+const jwtAuth = passport.authenticate('jwt', { session: false });
 
-//route to find user by jwt
-// app.post('/api/userSearch', jwtAuth, (req, res)=>{
-//   console.log(req.user)
-//   return req.user
-// })
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
 
-function runServer(databaseURL = DATABASE_URL, port = PORT) {
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
+
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseURL, (err) => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
       server = app.listen(port, () => {
-          console.log(`You're app is listening on ${port}`)
-          resolve()
-        })
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
         .on('error', err => {
-          mongoose.disconnect()
-          reject(err)
-        })
-    })
-  })
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
 }
 
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log('closing server')
+      console.log('Closing server');
       server.close(err => {
         if (err) {
-          return reject(err)
+          return reject(err);
         }
-        resolve()
-      })
-    })
-  })
+        resolve();
+      });
+    });
+  });
 }
-
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err))
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = {
-  runServer, app, closeServer
-}
+module.exports = { app, runServer, closeServer };
