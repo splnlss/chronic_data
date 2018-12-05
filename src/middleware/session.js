4c6cd5cc1545afca5b51e02eea5195c459020918
splnlss <jon@splnlss.com>
2018-12-05 12:40:04 -0500

    removed session depreciation warnings

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
   const session = require('express-session')
   app.use(session({
-    secret: 'shhhhhhh'
+    secret: 'shhhhhhh',
+    cookie: { maxAge: 60000 },
+    resave: true,
+    saveUninitialized: true
   }))
 }
